3ed9ae5f1cb13fc3aed100d24c46ae84e35fc172
Savaliya-Anvr <ravi.s@anvtech.co>
2024-01-16 15:45:58 +0530

    adding new file

--- OOPs in python.py
+++ OOPs in python.py
@@ -3,20 +3,27 @@ class First:
         self.name = name
         self.age = age
         self.prof = prof
+        self.Second = Second()
+        self.Third = Third()
+        print(self.Second.lname, self.Third.timar)
 
     def details(self):
         print(self.name + ' is ' + str(self.age) + ' years old and working as '+ self.prof)
 
 class Second(First):
-    def __init__(self,name,age,prof,lname):
-        self.lname = lname
-        First.__init__(self,name,age,prof)
+    def __init__(self):
+        self.lname = 'noob'
+
 
     def full(self):
         print('N the Last name is ' + self.lname , self.name)
 
-abc = First('siut',457,'hoyer')
-ab = Second('sut',45,'toyer','suari')
-ab.full()
-ab.details()
+
+class Third:
+    def __init__(self):
+        self.timar = 'yuta 90'
+abc = First('ssiya',457,'waiter')
+# ab = Second()
+# ab.full()
+# ab.details()
 abc.details()
