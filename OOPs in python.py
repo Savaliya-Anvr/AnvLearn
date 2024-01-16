96daf6f0582d5477b5d085977c2cd296347915ae
Savaliya-Anvr <ravi.s@anvtech.co>
2024-01-16 15:54:39 +0530

    adding new file

--- OOPs in python.py
+++ OOPs in python.py
@@ -1,5 +1,5 @@
 class First:
-    def __init__(self,name,age,prof):
+    def __init__(self, name, age, prof):
         self.name = name
         self.age = age
         self.prof = prof
@@ -8,22 +8,28 @@ class First:
         print(self.Second.lname, self.Third.timar)
 
     def details(self):
-        print(self.name + ' is ' + str(self.age) + ' years old and working as '+ self.prof)
+        print(self.name + ' is ' + str(self.age) + ' years old and working as ' + self.prof)
+
 
 class Second(First):
     def __init__(self):
         self.lname = 'noob'
 
-
     def full(self):
-        print('N the Last name is ' + self.lname , self.name)
+        print('N the Last name is ' + self.lname, self.name)
 
 
 class Third:
     def __init__(self):
         self.timar = 'yuta 90'
-abc = First('ssiya',457,'waiter')
+
+
+abc = First('Ziya', 457, 'waiter')
 # ab = Second()
 # ab.full()
 # ab.details()
 abc.details()
+
+with open("spare.txt") as f:
+    print(f.read())
+    f.close()
