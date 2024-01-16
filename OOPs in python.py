51f8b9a3aac0a355065cfd4ac853a7238fa62d55
Savaliya-Anvr <ravi.s@anvtech.co>
2024-01-16 12:12:26 +0530

    adding new file

--- OOPs in python.py
+++ OOPs in python.py
@@ -8,15 +8,15 @@ class First:
         print(self.name + ' is ' + str(self.age) + ' years old and working as '+ self.prof)
 
 class Second(First):
-    def __init__(self,name,age,prof):
-        # self.lname = lname
+    def __init__(self,name,age,prof,lname):
+        self.lname = lname
         First.__init__(self,name,age,prof)
 
     def full(self):
-        print('N the Last name is ' + self.prof , self.name)
+        print('N the Last name is ' + self.lname , self.name)
 
-# abc = First()
-ab = Second('sut',45,'toyer')
+abc = First('siut',457,'hoyer')
+ab = Second('sut',45,'toyer','suari')
 ab.full()
 ab.details()
-# abc.details()
+abc.details()
